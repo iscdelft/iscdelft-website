82dd99dd675033b3834195df5b70927ebf54ad6a
Mithun Martin Mendez <mithun@themarblejar.io>
2023-02-06 16:24:11 +0100

    added 2021 and 2022 financial statement

--- src/pages/anbi/index.js
+++ src/pages/anbi/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Layout from "../../components/Layout";
 
-import financialStatement from "../../img/Bookyear_2020.pdf"
+import financialStatement1 from "../../img/Bookyear_2020.pdf"
+import financialStatement2 from "../../img/Bookyear_2021.pdf"
+import financialStatement3 from "../../img/Bookyear_2022.pdf"
 
 const ANBI = () => (
   <Layout>
@@ -10,7 +12,9 @@ const ANBI = () => (
       <p style={{ marginBottom: 30}}>
         Financial Statements:
       </p>
-      <a className="link-button" target="_blank" href={financialStatement}>Boekjaar 2020 Stichting Internationaal Studenten Pastoraat</a>
+      <a className="link-button" target="_blank" href={financialStatement1}>Boekjaar 2020 Stichting Internationaal Studenten Pastoraat</a>
+      <a className="link-button" target="_blank" href={financialStatement2}>Boekjaar 2021 Stichting Internationaal Studenten Pastoraat</a>
+      <a className="link-button" target="_blank" href={financialStatement3}>Boekjaar 2022 Stichting Internationaal Studenten Pastoraat</a>
     </div>
   </Layout>
 );
